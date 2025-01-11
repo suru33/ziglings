d833baed7b4973e07f9ae4851b84ece0d20666e9
SuRu <33urus+github@gmail.com>
2025-01-11 07:47:39 +0200

    016 to 052

--- exercises/032_unreachable.zig
+++ exercises/032_unreachable.zig
@@ -21,7 +21,6 @@ const std = @import("std");
 
 pub fn main() void {
     const operations = [_]u8{ 1, 1, 1, 3, 2, 2 };
-
     var current_value: u32 = 0;
 
     for (operations) |op| {
@@ -35,6 +34,7 @@ pub fn main() void {
             3 => {
                 current_value *= current_value;
             },
+            else => unreachable,
         }
 
         std.debug.print("{} ", .{current_value});
